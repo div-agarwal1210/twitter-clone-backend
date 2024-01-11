206404f1a364d121887edca111b9262e97f2d6ad
Divyansh Agarwal <divyansh.agarwal900@gmail.com>
2024-01-11 13:07:36 +0530

    backend commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://Twitter_admin:8sTXNlEhuT6zg91x@twitter.pw2ojag.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const postCollection = client.db("database").collection("posts"); // this collection is for team-ekt
+        const userCollection = client.db("database").collection("users"); // this collection is for team-srv
+
+        // get
+        app.get('/user', async (req, res) => {
+            const user = await userCollection.find().toArray();
+            res.send(user);
+        })
+        app.get('/loggedInUser', async (req, res) => {
+            const email = req.query.email;
+            const user = await userCollection.find({ email:email }).toArray();
+            res.send(user);
+        })
+        app.get('/post', async (req, res) => {
+            const post = (await postCollection.find().toArray()).reverse();
+            res.send(post);
+        })
+        app.get('/userPost', async (req, res) => {
+            const email = req.query.email;
+            const post = (await postCollection.find({ email: email }).toArray()).reverse();
+            res.send(post);
+        })
+
+        // post
+        app.post('/register', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.post('/post', async (req, res) => {
+            const post = req.body;
+            const result = await postCollection.insertOne(post);
+            res.send(result);
+        })
+
+        //patch
+        app.patch('/userUpdates/:email', async (req, res) => {
+            const filter = req.params;
+            const profile = req.body;
+            const options = { upsert: true };
+            const updateDoc = { $set: profile };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
+
+        console.log("connected");
+
+    } catch (error) {
+        console.log(error);
+    }
+} run().catch(console.dir);
+
+const port = process.env.portort || 8000;
+app.get('/', (req, res) => {
+    res.send("Hello World from me")
+})
+
+app.listen(port, () => {
+    console.log(`Example app is listen on ${port}`);
+})
+
+//?directConnection=true
